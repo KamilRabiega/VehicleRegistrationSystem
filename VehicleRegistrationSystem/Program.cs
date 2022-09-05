746cfe473962ae14e324c128e477e39803ce5daf
KamilekGitHub <63099679+KamilekGitHub@users.noreply.github.com>
2022-09-05 18:59:21 +0200

    Updated Main

--- VehicleRegistrationSystem/Program.cs
+++ VehicleRegistrationSystem/Program.cs
@@ -8,54 +8,93 @@ Console.WriteLine("Press 1 to register new vehicle to our system.");
 Console.WriteLine("Press 2 to search by license plate.");
 Console.WriteLine("Press 3 to display information about all vehicles in our system.");
 Console.WriteLine("Press 4 to search by last name.");
+Console.WriteLine("Press x button to exit.");
 
 var menuInput = Console.ReadLine();
 var registrationBook = new RegistrationBook();
 
-switch(menuInput)
+while(true)
 {
-    case "1":
-        Console.WriteLine("Adding a new vehice to our system.");
+    switch (menuInput)
+    {
+        case "1":
+            Console.WriteLine("Adding a new vehice to our system.");
+            Console.WriteLine("");
 
-        Console.WriteLine("Please type id: ");
-        var id = Int32.Parse(Console.ReadLine());
-        Console.WriteLine("Please type License Plate: ");
-        var plate = Console.ReadLine();
-        Console.WriteLine("Please type first name: ");
-        var fname = Console.ReadLine();
-        Console.WriteLine("Please type last name: ");
-        var lname = Console.ReadLine();
-        Console.WriteLine("Please type manufacturer: ");
-        var manufacturer = Console.ReadLine();
-        Console.WriteLine("Please type model: ");
-        var model = Console.ReadLine();
-        Console.WriteLine("Please type color: ");
-        var color = Console.ReadLine();
-        Console.WriteLine("Please type engine: ");
-        var engine = Console.ReadLine();
-        Console.WriteLine("Please type HP: ");
-        var horsepower = Console.ReadLine();
+            Console.WriteLine("Please type id: ");
+            var id = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Please type License Plate: ");
+            var plate = Console.ReadLine();
+            Console.WriteLine("Please type first name: ");
+            var fname = Console.ReadLine();
+            Console.WriteLine("Please type last name: ");
+            var lname = Console.ReadLine();
+            Console.WriteLine("Please type manufacturer: ");
+            var manufacturer = Console.ReadLine();
+            Console.WriteLine("Please type model: ");
+            var model = Console.ReadLine();
+            Console.WriteLine("Please type color: ");
+            var color = Console.ReadLine();
+            Console.WriteLine("Please type engine: ");
+            var engine = Console.ReadLine();
+            Console.WriteLine("Please type HP: ");
+            var horsepower = Console.ReadLine();
 
-        var newVehicle = new Vehicle(id, plate, fname, lname, manufacturer, model, color, engine, horsepower);
+            var newVehicle = new Vehicle(id, plate, fname, lname, manufacturer, model, color, engine, horsepower);
 
-        registrationBook.AddVehicle(newVehicle);
+            registrationBook.AddVehicle(newVehicle);
 
-        registrationBook.DisplayVehicleDetails(newVehicle);
-        break;
+            Console.WriteLine("");
+            Console.WriteLine("A new vehicle has been successfully added to the system.");
+            registrationBook.DisplayVehicleDetails(newVehicle);
+            Console.WriteLine("");
+            break;
 
-    case "2":
+        case "2":
+            Console.WriteLine("Searching by license plate.");
+            Console.WriteLine("");
+            Console.WriteLine("Type license plate: ");
+            var plateSearch = Console.ReadLine();
 
-        break;
+            Console.WriteLine("");
+            Console.WriteLine("The vehicle was found using the license plate.");
+            registrationBook.DisplayVehicles(plateSearch);
+            Console.WriteLine("");
+            break;
 
-    case "3":
+        case "3":
+            Console.WriteLine("");
+            Console.WriteLine("All vehicles from our system.");
 
-        break;
+            Console.WriteLine("");
+            registrationBook.DisplayAllVehicles();
+            Console.WriteLine("");
 
-    case "4":
+            break;
 
-        break;
+        case "4":
+            Console.WriteLine("");
+            Console.WriteLine("Searching by owner last name.");
+            Console.WriteLine("");
+            Console.WriteLine("Type last name: ");
 
-    default:
-        Console.WriteLine("You need to type numbers 1-4, invalid number.");
-        break;
+            var inputLastName = Console.ReadLine();
+
+            registrationBook.DisplayByLastName(inputLastName);
+
+            break;
+
+        case "x":
+            return;
+
+        default:
+            Console.WriteLine("You need to type numbers 1-4, invalid number.");
+            break;
+    }
+    Console.WriteLine("Press 1 to register new vehicle to our system.");
+    Console.WriteLine("Press 2 to search by license plate.");
+    Console.WriteLine("Press 3 to display information about all vehicles in our system.");
+    Console.WriteLine("Press 4 to search by last name.");
+    Console.WriteLine("Press x button to exit.");
+    menuInput = Console.ReadLine();
 }
